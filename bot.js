5720bc1df9a9bd0721629713590b03f0e561e750
Fofodiscord <42048326+discordyou23@users.noreply.github.com>
2018-08-31 03:22:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ client.on('ready', () => {
 
 
 
+
 client.on('ready', () => {                           
 client.user.setGame(`4music`,'https://www.twitch.tv/fofodiscord');                                                                                                                                                                                                                                                                                                                                                                                                                            
 });
