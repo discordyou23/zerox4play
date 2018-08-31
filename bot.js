93f8b3d532e5f394d81d32829ce39a86cad4a975
Fofodiscord <42048326+discordyou23@users.noreply.github.com>
2018-08-31 17:34:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,10 +21,9 @@ const devs = ['382889731316514826'];
 client.on('message', message => {
 if(message.content === adminprefix + "restart") {
       if (!devs.includes(message.author.id)) return;
-          message.channel.send(⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
         console.log(⚠️ جاري اعادة تشغيل البوت... ⚠️);
         client.destroy();
-        child_process.fork(__dirname + "/الملف.js");
+        child_process.fork(__dirname + "/bot.js");
         console.log(تم اعادة تشغيل البوت);
     }
   
